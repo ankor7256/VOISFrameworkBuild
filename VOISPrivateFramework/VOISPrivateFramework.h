c83c8c8e7b56592c477b42a26a45646ac3bd8481
Коротков Андрей <andrey.korotkov@joy-dev.com>
2023-02-01 15:15:33 -0500

    Initial Commit

---
+++ VOISPrivateFramework/VOISPrivateFramework.h
@@ -0,0 +1,18 @@
+//
+//  VOISPrivateFramework.h
+//  VOISPrivateFramework
+//
+//  Created by Andrew K on 01.02.23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VOISPrivateFramework.
+FOUNDATION_EXPORT double VOISPrivateFrameworkVersionNumber;
+
+//! Project version string for VOISPrivateFramework.
+FOUNDATION_EXPORT const unsigned char VOISPrivateFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VOISPrivateFramework/PublicHeader.h>
+
+
